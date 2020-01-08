5984a32fb7c42da8439d80d3ec13dd61dc023a49
bh90210 <ktc@protonmail.com>
2020-01-08 14:15:23 +0200

    update fix

--- update.go
+++ update.go
@@ -2,30 +2,19 @@ package cli
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 )
 
 func updateGocomu() error {
 	fmt.Println("updating")
-	// cmd := exec.Command("go", "get", "-u", "github.com/gocomu/cli")
-	// cmd.Env = os.Environ()
-	// cmd.Env = append(cmd.Env, "GO111MODULE=off")
-	// if err := cmd.Run(); err != nil {
-	// 	return err
-	// }
-
-	// dir, _ := os.UserHomeDir()
-	// os.Chdir(dir)
-	// os.Mkdir(".gocomutmp", 0755)
-	// os.Chdir(".gocomutpm")
-	cmd := exec.Command("gocomuUpdate")
-	// cmd.Env = os.Environ()
-	// cmd.Env = append(cmd.Env, "GO111MODULE=off")
+	cmd := exec.Command("go", "get", "-u", "github.com/gocomu/cli/cmd/gocomu")
+	cmd.Env = os.Environ()
+	cmd.Env = append(cmd.Env, "GO111MODULE=off")
 	if err := cmd.Run(); err != nil {
 		return err
 	}
-	// os.Chdir(dir)
-	// os.RemoveAll(".gocomutmp")
+
 	fmt.Println("Done!")
 	return nil
 }
