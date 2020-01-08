24a49b2cf7890027afb8889caa0a7bbb6ea84dba
bh90210 <ktc@protonmail.com>
2020-01-08 12:56:33 +0200

    update fix

--- update.go
+++ update.go
@@ -2,12 +2,13 @@ package cli
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 )
 
 func updateGocomu() error {
 	fmt.Println("updating")
-	// run go mod tidy
+	os.Setenv("GO111MODULE", "off")
 	cmd := exec.Command("go", "get", "-u", "github.com/gocomu/cli")
 	err := cmd.Run()
 	if err != nil {
