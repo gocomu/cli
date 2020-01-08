d68c5afcb254e64ad820100acbf48cf03650ca0c
bh90210 <ktc@protonmail.com>
2020-01-08 13:54:47 +0200

    test CI

--- cmd/gocomu/main.go
+++ cmd/gocomu/main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	// Create new cli
-	cli := clir.NewCli("GOCOMU", "GOCOMU CLI", "v0.0.11")
+	cli := clir.NewCli("GOCOMU", "GOCOMU CLI", "v0.0.12")
 
 	gocomu.ClirActions(cli)
 
--- update.go
+++ update.go
@@ -8,15 +8,18 @@ import (
 
 func updateGocomu() error {
 	fmt.Println("updating")
-	dir, _ := os.UserHomeDir()
-	os.Mkdir(dir+"/.gocomutmp", 0755)
+	// dir, _ := os.UserHomeDir()
+	// os.Chdir(dir)
+	// os.Mkdir(".gocomutmp", 0755)
+	// os.Chdir(".gocomutpm")
 	cmd := exec.Command("go", "get", "-u", "github.com/gocomu/cli")
 	cmd.Env = os.Environ()
-	cmd.Env = append(cmd.Env, "GO111MODULE=off")
+	cmd.Env = append(cmd.Env, "GO111MODULE=on")
 	if err := cmd.Run(); err != nil {
 		return err
 	}
-	os.RemoveAll(dir + "/.gocomutmp")
+	// os.Chdir(dir)
+	// os.RemoveAll(".gocomutmp")
 	fmt.Println("Done!")
 	return nil
 }
