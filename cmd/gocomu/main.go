8265f6096427b494dfc4d65a055d24eebec85fdc
bh90210 <ktc@protonmail.com>
2020-03-05 02:10:06 +0200

    fix: serve command, feat: auto version update CI

--- cmd/gocomu/main.go
+++ cmd/gocomu/main.go
@@ -4,20 +4,20 @@ import (
 	"fmt"
 
 	gocomu "github.com/gocomu/cli"
-	"github.com/gookit/color"
+	// release "github.com/gocomu/version"
 	"github.com/leaanthony/clir"
 )
 
 func main() {
 	// Create new cli
-	cli := clir.NewCli("GOCOMU", "GOCOMU CLI", "v0.0.14")
+	cli := clir.NewCli("GOCOMU", "GOCOMU CLI", gocomu.Version)
 
 	gocomu.ClirActions(cli)
 
 	// Run!
 	err := cli.Run()
 	if err != nil {
-		color.Warn.Println(fmt.Sprint(err))
+		fmt.Println(err)
 		return
 	}
 }
--- init.go
+++ init.go
@@ -10,12 +10,4 @@ var dir string
 func init() {
 	// assign global var dir to current working directory
 	dir, _ = os.Getwd()
-
-	// check if dir is a GOCOMU project top folder
-	// _, err := Yaml()
-	// if not print error and exit
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(1)
-	// }
 }
